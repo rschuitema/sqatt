4ec8238c9f7a313cc098d0ee0ed436d9e79db1dd
Robbert Schuitema <robbert.schuitema@gmail.com>
2019-09-28 08:33:27 +0200

    add comments

--- src/riskmatrix/risk_matrix.py
+++ src/riskmatrix/risk_matrix.py
@@ -1,8 +1,15 @@
+"""This module represent a matrix that defines quality thresholds for each risk quadrant and to which risk quadrant
+each component belongs."""
 import csv
 
 
 class RiskMatrix:
+    """Represents a matrix that defines quality thresholds for each risk quadrant and to which risk quadrant
+       each component belongs."""
+
     def __init__(self):
+        """Constructs a risk matrix."""
+
         self.metric_thresholds = {}
         self.component_risk_level = {}
         self.metric_comparators = {'<': RiskMatrix.metric_lt, '<=': RiskMatrix.metric_le, '>': RiskMatrix.metric_gt,
@@ -10,33 +17,43 @@ class RiskMatrix:
 
     @staticmethod
     def metric_lt(metric1, metric2):
+        """Method to compare if metric1 is less than metric2."""
         return metric1 < metric2
 
     @staticmethod
     def metric_le(metric1, metric2):
+        """Method to compare if metric1 is less than or equal to metric2."""
         return metric1 <= metric2
 
     @staticmethod
     def metric_gt(metric1, metric2):
+        """Method to compare if metric1 is greater than metric2."""
         return metric1 > metric2
 
     @staticmethod
     def metric_ge(metric1, metric2):
+        """Method to compare if metric1 is greater than or equal to metric2."""
         return metric1 >= metric2
 
     def add_component_risk_level(self, filename, csv_reader=None):
+        """Add risk level for each component."""
+
         with open(filename) as csv_file:
             reader = csv_reader or csv.DictReader(csv_file, skipinitialspace=True)
             for row in reader:
                 self.component_risk_level[row['Component']] = row['Quadrant']
 
     def add_metric_thresholds(self, filename, csv_reader=None):
+        """Add metric thresholds for each quadrant."""
+
         with open(filename) as csv_file:
             reader = csv_reader or csv.DictReader(csv_file, skipinitialspace=True)
             for row in reader:
                 self.metric_thresholds[row['Quadrant']] = row
 
     def verify_metric(self, filename, csv_reader=None):
+        """Verify if all the components comply to the specified metric threshold of its quadrant."""
+
         result = True
         with open(filename) as csv_file:
             reader = csv_reader or csv.reader(csv_file, skipinitialspace=True)
