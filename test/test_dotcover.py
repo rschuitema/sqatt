a3a45b7ebac5436b8b5b2b38e04c16ea30f5b50a
Robbert Schuitema <robbert.schuitema@gmail.com>
2019-09-29 08:43:10 +0200

    add test for create_report_directory

--- test/test_dotcover.py
+++ test/test_dotcover.py
@@ -5,7 +5,7 @@ import pytest
 import xmltodict
 
 from src.dotcover.dotcover import get_namespaces, determine_namespaces, determine_coverage_of_namespace, \
-    save_coverage_per_namespace
+    save_coverage_per_namespace, create_report_directory
 
 
 def test_get_namespaces_empty_input_returns_empty_set():
@@ -194,3 +194,39 @@ def test_save_coverage_per_namespace(csv_mock):
         mocked_file.assert_called_once_with(report_dir + r'coverage_per_namespace.csv', 'w')
 
         csv_mock.writer().assert_has_calls(calls)
+
+
+@patch('os.path.exists')
+def test_create_report_directory_directory_exists(path_exists_mock):
+    """Test that the create report directory does not create the directory when it already exists."""
+
+    # arrange
+
+    directory = r"c:\temp\reports"
+    path_exists_mock.return_value = True
+
+    # act
+    report_dir = create_report_directory(directory)
+
+    # assert
+    assert directory == report_dir
+    path_exists_mock.assert_called_once()
+
+
+@patch('os.makedirs')
+@patch('os.path.exists')
+def test_create_report_directory_directory_created(path_exists_mock, makedirs_mock):
+    """Test that the create report directory creates the directory when it does not exist."""
+
+    # arrange
+
+    directory = r"c:\temp\reports"
+    path_exists_mock.return_value = False
+
+    # act
+    report_dir = create_report_directory(directory)
+
+    # assert
+    assert directory == report_dir
+    path_exists_mock.assert_called_once()
+    makedirs_mock.assert_called_once()
