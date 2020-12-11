a67170711a32ac446cdb33c748a2746343d4026f
Robbert <robbert.schuitema@gmail.com>
2020-12-11 23:33:23 +0100

    reformat

--- test/test_subprocess.py
+++ test/test_subprocess.py
@@ -21,7 +21,7 @@ def test_subprocess_throws_exception_when_providing_command_as_plain_string():
 
 
 @mock.patch("shutil.which")
-def test_which_is_successful_with_correct_parameters(which_mock,):
+def test_which_is_successful_with_correct_parameters(which_mock):
     # Arrange
     command = ["test_command", "test_arguments"]
     which_mock.return_value = "/test_path/test_command"
@@ -36,7 +36,7 @@ def test_which_is_successful_with_correct_parameters(which_mock,):
 
 
 @mock.patch("shutil.which")
-def test_which_raises_exception_with_shutil_which_returning_none(which_mock,):
+def test_which_raises_exception_with_shutil_which_returning_none(which_mock):
     # Arrange
     command = ["unexisting_test_command", "test_arguments"]
     which_mock.return_value = None
@@ -51,9 +51,7 @@ def test_which_raises_exception_with_shutil_which_returning_none(which_mock,):
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
-def test_execute_is_successful_with_correct_command_parameters(
-    which_mock, subprocess_mock,
-):
+def test_execute_is_successful_with_correct_command_parameters(which_mock, subprocess_mock):
     # Arrange
     command = ["test_command", "test_arguments"]
     which_mock.return_value = "test_path/test_command"
@@ -66,14 +64,19 @@ def test_execute_is_successful_with_correct_command_parameters(
 
     # Assert
     subprocess_mock.assert_called_once_with(
-        ["test_path/test_command", "test_arguments"], stdout=DEVNULL, stderr=DEVNULL, shell=False, timeout=None,
+        ["test_path/test_command", "test_arguments"],
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+        shell=False,
+        timeout=None,
     )
 
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_standard_streams_are_modified_when_overridden(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -98,7 +101,8 @@ def test_standard_streams_are_modified_when_overridden(
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_runs_subprocess_with_stderr_output_with_verbose_is_2(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -112,14 +116,19 @@ def test_execute_runs_subprocess_with_stderr_output_with_verbose_is_2(
 
     # Assert
     subprocess_mock.assert_called_once_with(
-        ["test_path/test_command", "test_arguments"], stdout="stdout_stream", stderr=None, shell=False, timeout=None,
+        ["test_path/test_command", "test_arguments"],
+        stdout="stdout_stream",
+        stderr=None,
+        shell=False,
+        timeout=None,
     )
 
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_runs_subprocess_with_stdout_and_stderr_output_with_verbose_is_3(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -133,14 +142,19 @@ def test_execute_runs_subprocess_with_stdout_and_stderr_output_with_verbose_is_3
 
     # Assert
     subprocess_mock.assert_called_once_with(
-        ["test_path/test_command", "test_arguments"], stdout=None, stderr=None, shell=False, timeout=None,
+        ["test_path/test_command", "test_arguments"],
+        stdout=None,
+        stderr=None,
+        shell=False,
+        timeout=None,
     )
 
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_runs_subprocess_with_stdout_and_stderr_default_constructor_output_with_verbose_is_3(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -154,14 +168,19 @@ def test_execute_runs_subprocess_with_stdout_and_stderr_default_constructor_outp
 
     # Assert
     subprocess_mock.assert_called_once_with(
-        ["test_path/test_command", "test_arguments"], stdout=None, stderr=None, shell=False, timeout=None,
+        ["test_path/test_command", "test_arguments"],
+        stdout=None,
+        stderr=None,
+        shell=False,
+        timeout=None,
     )
 
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_runs_subprocess_with_stdout_and_stderr_output_with_verbose_is_greater_than_3(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -175,14 +194,19 @@ def test_execute_runs_subprocess_with_stdout_and_stderr_output_with_verbose_is_g
 
     # Assert
     subprocess_mock.assert_called_once_with(
-        ["test_path/test_command", "test_arguments"], stdout=None, stderr=None, shell=False, timeout=None,
+        ["test_path/test_command", "test_arguments"],
+        stdout=None,
+        stderr=None,
+        shell=False,
+        timeout=None,
     )
 
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_runs_subprocess_with_stdout_redirect_with_defined_stdout_stream_and_verbose_smaller_than_3(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -207,7 +231,8 @@ def test_execute_runs_subprocess_with_stdout_redirect_with_defined_stdout_stream
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_runs_subprocess_with_stdout_redirect_with_defined_stdout_stream_and_verbose_greater_than_3(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -221,14 +246,19 @@ def test_execute_runs_subprocess_with_stdout_redirect_with_defined_stdout_stream
 
     # Assert
     subprocess_mock.assert_called_once_with(
-        ["test_path/test_command", "test_arguments"], stdout=None, stderr=None, shell=False, timeout=None,
+        ["test_path/test_command", "test_arguments"],
+        stdout=None,
+        stderr=None,
+        shell=False,
+        timeout=None,
     )
 
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_runs_subprocess_with_correct_timeout_value_with_timeout_set(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -242,14 +272,19 @@ def test_execute_runs_subprocess_with_correct_timeout_value_with_timeout_set(
 
     # Assert
     subprocess_mock.assert_called_once_with(
-        ["test_path/test_command", "test_arguments"], stdout=DEVNULL, stderr=DEVNULL, shell=False, timeout=180,
+        ["test_path/test_command", "test_arguments"],
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+        shell=False,
+        timeout=180,
     )
 
 
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_raises_exception_with_subprocess_called_process_error_exception(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
@@ -267,7 +302,8 @@ def test_execute_raises_exception_with_subprocess_called_process_error_exception
 @mock.patch("src.facility.subprocess.check_call")
 @mock.patch("shutil.which")
 def test_execute_raises_exception_with_subprocess_timeout_expired_exception(
-    which_mock, subprocess_mock,
+    which_mock,
+    subprocess_mock,
 ):
     # Arrange
     command = ["test_command", "test_arguments"]
