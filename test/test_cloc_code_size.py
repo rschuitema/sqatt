4fbeb89c74e6baad713e14679a23d66f2e6feb69
Robbert Schuitema <robbert.schuitema@gmail.com>
2020-12-25 21:36:14 +0100

    fix formatting and style

--- test/test_cloc_code_size.py
+++ test/test_cloc_code_size.py
@@ -41,13 +41,18 @@ def test_write_header(csv_mock):
 def test_write_code_size_metrics(csv_mock):
     """Test that the correct metrics are written to the csv file."""
 
-    metrics = {"Java": {"files": 201, "blank": 20, "code": 1003, "comment": 230},
-               "C": {"files": 100, "blank": 7, "code": 1220, "comment": 30},
-               "SUM": {"files": 301, "blank": 7, "code": 2120, "comment": 130}}
+    metrics = {
+        "Java": {"files": 201, "blank": 20, "code": 1003, "comment": 230},
+        "C": {"files": 100, "blank": 7, "code": 1220, "comment": 30},
+        "SUM": {"files": 301, "blank": 7, "code": 2120, "comment": 130}
+    }
 
     csv_mock.writer = Mock(writerow=Mock())
-    calls = [call.writerow(["Java", 201, 20, 1003, 230]), call.writerow(["C", 100, 7, 1220, 30]),
-             call.writerow(["SUM", 301, 7, 2120, 130])]
+    calls = [
+        call.writerow(["Java", 201, 20, 1003, 230]),
+        call.writerow(["C", 100, 7, 1220, 30]),
+        call.writerow(["SUM", 301, 7, 2120, 130])
+    ]
     write_code_size_metrics(csv_mock.writer, metrics)
 
     csv_mock.writer.writerow.assert_has_calls(calls)
@@ -62,10 +67,10 @@ def test_get_size_metrics():
     SUM, 100, 200, 300, 400"""
     )
 
-    report_file_name = r'code_sie_report.csv'
+    report_file_name = r"code_sie_report.csv"
     test_reader = csv.DictReader(data, delimiter=",", skipinitialspace=True)
     with patch("src.cloc.cloc_code_size.open", mock_open()) as mocked_file:
         metrics = get_size_metrics(report_file_name, test_reader)
 
-    mocked_file.assert_called_once_with(report_file_name, 'r', newline='\n')
-    assert metrics["SUM"]["code"] == '400'
+    mocked_file.assert_called_once_with(report_file_name, "r", newline="\n")
+    assert metrics["SUM"]["code"] == "400"
