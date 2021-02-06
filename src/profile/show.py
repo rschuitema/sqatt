c3816ea6cafd76d3f6c72a0be572951bb307e4ae
Robbert Schuitema <robbert.schuitema@gmail.com>
2021-02-06 10:05:17 +0100

    split the creation and displaying of the figure into two functions

--- src/profile/show.py
+++ src/profile/show.py
@@ -2,11 +2,21 @@
 
 import plotly.graph_objects as go
 
-profile_colors = ["rgb(121, 185, 79)", "rgb(255, 204, 5)", "rgb(251, 135, 56)", "rgb(204, 5, 5)", "rgb(121,55,171)"]
+profile_colors = [
+    "rgb(121, 185, 79)",
+    "rgb(255, 204, 5)",
+    "rgb(251, 135, 56)",
+    "rgb(204, 5, 5)",
+    "rgb(121,55,171)",
+    "rgb(255, 127, 237)",
+    "rgb(127, 51, 0)",
+    "rgb(0, 127, 14)",
+    "rgb(0, 38, 255)",
+]
 
 
-def show_profile(profile):
-    """Show the profile in a donut."""
+def profile_figure(profile):
+    """Create the profile donut."""
 
     labels = []
     values = []
@@ -29,4 +39,11 @@ def show_profile(profile):
 
     fig.update_layout(legend=dict(orientation="h", yanchor="bottom", xanchor="center", x=0.5))
 
+    return fig
+
+
+def show_profile(profile):
+    """Show the profile in a donut."""
+
+    fig = profile_figure(profile)
     fig.show()
