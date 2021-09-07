5c59139d8cdd1ed68559d95284c2c1dcbfa9e2a4
Robbert Schuitema <robbert.schuitema@gmail.com>
2021-09-07 07:17:46 +0200

    add setup_report_directory function

--- src/reporting/reporting.py
+++ src/reporting/reporting.py
@@ -1,5 +1,6 @@
 """Report utility functions."""
 import os
+import shutil
 
 
 def create_report_directory(directory):
@@ -7,4 +8,15 @@ def create_report_directory(directory):
 
     if not os.path.exists(directory):
         os.makedirs(directory)
+    return directory
+
+
+def setup_report_directory(directory):
+    """Create an empty report directory."""
+
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+
+    os.makedirs(directory)
+
     return directory
