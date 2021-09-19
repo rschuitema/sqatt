92990b8853dae1fffb22f0b52ec5d489e0cdd1cb
Robbert Schuitema <robbert.schuitema@gmail.com>
2021-09-19 11:53:37 +0200

    add test for measure lines of code

--- test/test_cloc_measure.py
+++ test/test_cloc_measure.py
@@ -3,11 +3,11 @@ import csv
 from io import StringIO
 from unittest.mock import patch, mock_open
 
-from src.cloc.cloc_measure import get_size_metrics
+import src.facility.subprocess
+from src.cloc.cloc_measure import get_size_metrics, measure_lines_of_code
 
 
 def test_get_size_metrics():
-
     data = StringIO(
         """language, files, blank, comment, code
         Java, 10, 11, 12, 13
@@ -22,3 +22,30 @@ def test_get_size_metrics():
 
     mocked_file.assert_called_once_with(report_file_name, "r", newline="\n", encoding="utf-8")
     assert metrics["C"]["code"] == "23"
+
+
+@patch("src.cloc.cloc_measure.Subprocess")
+def test_measure_loc_calls_cloc_with_correct_parameters(subprocess_mock):
+    """Test that cloc is called with the correct parameters to measure the lines of code."""
+
+    # arrange
+    input_dir = "/test_root/test_dir"
+    report_file = "my_report"
+    measure_filter = "my_filter"
+
+    # act
+    measure_lines_of_code(input_dir, report_file, measure_filter)
+
+    # assert
+    subprocess_mock.assert_called_with(
+        [
+            "cloc",
+            "--csv",
+            "--csv-delimiter=,",
+            "--hide-rate",
+            f"--report-file={report_file}",
+            measure_filter,
+            input_dir,
+        ],
+        verbose=1,
+    )
