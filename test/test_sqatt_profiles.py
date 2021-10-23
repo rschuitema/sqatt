b0b8a252355df8aa057ee88df729d5c5ba1ca450
Robbert Schuitema <robbert.schuitema@gmail.com>
2021-10-23 16:30:31 +0200

    remove duplicate code

--- test/test_sqatt_profiles.py
+++ test/test_sqatt_profiles.py
@@ -13,94 +13,75 @@ from src.profile.sqatt_profiles import (
 def test_create_function_size_profile():
     """Test the creation of the function size profile."""
 
-    # arrange
-
     # act
     profile = create_function_size_profile()
 
     # assert
     assert profile.name() == "Function size"
-    assert profile.regions()[0].loc() == 0
-    assert profile.regions()[1].loc() == 0
-    assert profile.regions()[2].loc() == 0
-    assert profile.regions()[3].loc() == 0
+    assert that_profile_has_no_lines_of_code(profile)
 
 
 def test_create_complexity_profile():
     """Test the creation of the complexity profile."""
-
-    # arrange
 
     # act
     profile = create_complexity_profile()
 
     # assert
     assert profile.name() == "Complexity"
-    assert profile.regions()[0].loc() == 0
-    assert profile.regions()[1].loc() == 0
-    assert profile.regions()[2].loc() == 0
-    assert profile.regions()[3].loc() == 0
+    assert that_profile_has_no_lines_of_code(profile)
 
 
 def test_create_function_parameters_profile():
     """Test the creation of the function parameters profile."""
-
-    # arrange
 
     # act
     profile = create_function_parameters_profile()
 
     # assert
     assert profile.name() == "Function parameters"
-    assert profile.regions()[0].loc() == 0
-    assert profile.regions()[1].loc() == 0
-    assert profile.regions()[2].loc() == 0
-    assert profile.regions()[3].loc() == 0
+    assert that_profile_has_no_lines_of_code(profile)
 
 
 def test_create_fan_in_profile():
     """Test the creation of the fan in profile."""
-
-    # arrange
 
     # act
     profile = create_fan_in_profile()
 
     # assert
     assert profile.name() == "Fan in"
-    assert profile.regions()[0].loc() == 0
-    assert profile.regions()[1].loc() == 0
-    assert profile.regions()[2].loc() == 0
-    assert profile.regions()[3].loc() == 0
+    assert that_profile_has_no_lines_of_code(profile)
 
 
 def test_create_fan_out_profile():
     """Test the creation of the fan out profile."""
-
-    # arrange
 
     # act
     profile = create_fan_out_profile()
 
     # assert
     assert profile.name() == "Fan out"
-    assert profile.regions()[0].loc() == 0
-    assert profile.regions()[1].loc() == 0
-    assert profile.regions()[2].loc() == 0
-    assert profile.regions()[3].loc() == 0
+    assert that_profile_has_no_lines_of_code(profile)
 
 
 def test_create_file_size_profile():
     """Test the creation of the file size profile."""
-
-    # arrange
 
     # act
     profile = create_file_size_profile()
 
     # assert
     assert profile.name() == "File size"
-    assert profile.regions()[0].loc() == 0
-    assert profile.regions()[1].loc() == 0
-    assert profile.regions()[2].loc() == 0
-    assert profile.regions()[3].loc() == 0
+    assert that_profile_has_no_lines_of_code(profile)
+
+
+def that_profile_has_no_lines_of_code(profile):
+    """Check that a profile has no lines of code."""
+
+    result = profile.regions()[0].loc() == 0
+    result = result and profile.regions()[1].loc() == 0
+    result = result and profile.regions()[2].loc() == 0
+    result = result and profile.regions()[3].loc() == 0
+
+    return result
